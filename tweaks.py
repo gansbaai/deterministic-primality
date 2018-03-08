1209730586bff09387c94cbbb7f225a9b2d4725a
Kurt Kirschner <gansbaai@users.noreply.github.com>
2018-03-08 02:41:56 -0800

    Update tweaks.py

--- tweaks.py
+++ tweaks.py
@@ -1,6 +1,5 @@
 def primality(p):
     polynomial = np.poly1d(np.array([1,-1], dtype=object)) ** p
-    coefficients = polynomial.c
-    coefficients = (list(coefficients).pop(0).pop(-1))
+    coefficients = (list(polynomial.c).pop(0).pop(-1))
     prime = all(c % p == 0 for c in coefficients)
     return prime
